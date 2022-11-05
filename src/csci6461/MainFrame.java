d252899f7f1ee808bbb9a3750eecc4f45b574484
VQSHR <stoneliuicloud@icloud.com>
2022-11-04 22:00:37 -0400

    random number

--- src/csci6461/MainFrame.java
+++ src/csci6461/MainFrame.java
@@ -553,7 +553,7 @@ public class MainFrame extends JFrame {
 				simulator.init("./src/csci6461/program1.txt");
 				if (textField_Keyboard.getText().isEmpty()) {
 					setPrinter("using default value...\n");
-					textField_Keyboard.setText("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22");
+					textField_Keyboard.setText("17827,8255,23642,1412,870,3177,2639,231,2899,18,1109,31,88,973,100,1281,1241,713,119,240,713");
 				}
 					
 				String msg = "Array of 20 integers:\n";
